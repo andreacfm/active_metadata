c238ea17196322c58da61acc84e8fc1378f46ed5
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-20 11:30:29 +0200

    Added filtering of the notes for the current model

--- spec/active_metadata_spec.rb
+++ spec/active_metadata_spec.rb
@@ -12,15 +12,16 @@ describe ActiveMetadata do
 
   context "model methods" do
 
+    before(:each) do
+      @document = Document.create! { |d| d.name = "John" }
+      @document.reload
+    end
+
     it "should exist a method act_as_metadata in the model" do
       Document.respond_to?(:act_as_metadata).should be_true
     end
 
-    context "managing notes" do
-
-      before(:each) do
-        @document = Document.create(:name => "John")
-      end
+    context "definig notes methods" do
 
       it "should respond to create_note_for_name" do
         @document.respond_to?(:create_note_for_name).should be_true
@@ -37,7 +38,9 @@ describe ActiveMetadata do
       it "should respond to notes_for_name=" do
         @document.respond_to?(:notes_for_name=).should be_true
       end
+    end
 
+    context "saving and quering notes" do
       it "should create a new note for a given field" do
         @document.create_note_for_name("Very important note!")
         @document.notes_for_name.should have(1).record
@@ -45,6 +48,18 @@ describe ActiveMetadata do
 
       it "should verify the content of a note created" do
         @document.create_note_for_name("Very important note!")
+        @document.notes_for_name.last["note"].should eq "Very important note!"
+      end
+
+      it "should verify that notes_for_name return only notes for the self Document" do
+        # fixtures
+        @another_doc = Document.create :name => "Andrea"
+        @another_doc.create_note_for_name("Very important note for doc2!")
+        @another_doc.reload
+        @document.create_note_for_name("Very important note!")
+
+          # expectations
+        @document.notes_for_name.count.should eq(1)
         @document.notes_for_name.last["note"].should eq "Very important note!"
       end
 
