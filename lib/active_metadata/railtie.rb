ef9efcb08c562c3a83e945f03d3e2581ed92d7b4
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-20 16:56:58 +0200

    railtie

--- lib/active_metadata/railtie.rb
+++ lib/active_metadata/railtie.rb
@@ -1,8 +1,6 @@
 require 'rails'
-module ActiveMetadata
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      require_relative '../rake/rake.task'
-    end
+class Railtie < Rails::Railtie
+  rake_tasks do
+    require_relative '../rake/rake.task'
   end
 end
