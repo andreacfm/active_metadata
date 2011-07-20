88957164242e15b969060a0136cbdc3596ed04f5
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-07-20 15:10:33 +0200

    Deleted useless lines

--- spec/active_metadata_spec.rb
+++ spec/active_metadata_spec.rb
@@ -2,14 +2,6 @@ require "spec_helper"
 
 describe ActiveMetadata do
 
-  # pr = pratica.create :name => 'Andrea'
-  #
-  # pr.name_create_nota ('nota') # crea una nuova nota
-  # pr.name_note.should have(1).record
-  #
-  # pr.nome_history
-  # pr.nome_note
-
   context "model methods" do
 
     before(:each) do
