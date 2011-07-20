2104fce75e30f822b66f871574b285f96e60e681
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-20 10:51:06 +0200

    refactoring names notes

--- spec/active_metadata_spec.rb
+++ spec/active_metadata_spec.rb
@@ -22,30 +22,30 @@ describe ActiveMetadata do
         @document = Document.create(:name => "John")
       end
 
-      it "should respond to create_nota_for_name" do
-        @document.respond_to?(:create_nota_for_name).should be_true
+      it "should respond to create_note_for_name" do
+        @document.respond_to?(:create_note_for_name).should be_true
       end
 
-      it "should respond to update_nota_for_name" do
-        @document.respond_to?(:update_nota_for_name).should be_true
+      it "should respond to update_note_for_name" do
+        @document.respond_to?(:update_note_for_name).should be_true
       end
 
-      it "should respond to note_for_name" do
-        @document.respond_to?(:note_for_name).should be_true
+      it "should respond to notes_for_name" do
+        @document.respond_to?(:notes_for_name).should be_true
       end
 
-      it "should respond to note_for_name=" do
-        @document.respond_to?(:note_for_name=).should be_true
+      it "should respond to notes_for_name=" do
+        @document.respond_to?(:notes_for_name=).should be_true
       end
 
       it "should create a new note for a given field" do
-        @document.create_nota_for_name("Very important note!")
-        @document.note_for_name.should have(1).record
+        @document.create_note_for_name("Very important note!")
+        @document.notes_for_name.should have(1).record
       end
 
       it "should verify the content of a note created" do
-        @document.create_nota_for_name("Very important note!")
-        @document.note_for_name.last["note"].should eq "Very important note!"
+        @document.create_note_for_name("Very important note!")
+        @document.notes_for_name.last["note"].should eq "Very important note!"
       end
 
     end
