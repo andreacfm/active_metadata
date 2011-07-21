52194c7f55687fe51ad19496aa86065c30bb5619
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-21 11:34:16 +0200

    history implemetation and tests

--- lib/active_metadata.rb
+++ lib/active_metadata.rb
@@ -5,16 +5,27 @@ require "active_metadata/railtie" if defined?(Rails)
 
 module ActiveMetadata
 
+  HISTORY_SKIPS = ['id','created_at','updated_at']
+
   env = defined?(Rails) ? Rails.env : ENV['DATABASE_ENV']
   unless env.nil?
     CONFIG = YAML.load_file('config/mongo.yml')[env]
     CONNECTION = Mongo::Connection.new(CONFIG['host'],CONFIG['port']).db CONFIG['database']
   end
 
+  def self.included(base)
+    base.after_save :save_history
+  end
+
   def self.notes
     CONNECTION['notes']
   end
 
+  def self.history
+    CONNECTION['history']
+  end
+
+  # NOTES
   def create_note_for(field, note, created_by=nil)    
     raise RuntimeError, "The object id MUST be valued" unless self.id
 
@@ -33,6 +44,17 @@ module ActiveMetadata
     notes.each do |note|
       create_note_for field,note
     end
+  end
+
+  # History
+  def save_history
+    self.changes.each do |key,value| next if HISTORY_SKIPS.include?(key)
+      ActiveMetadata.history.insert :id => self.id, :field => key, :value => value[1], :created_at => Time.now.utc
+    end
+  end
+
+  def history_for field
+    ActiveMetadata.history.find({:id => self.id, :field => field}).to_a
   end
 
 end
