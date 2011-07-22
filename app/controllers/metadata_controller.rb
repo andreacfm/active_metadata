f93d80739dca25165fbc94ef16364cddf4f63978
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-07-22 11:56:26 +0200

    Added controller and views

---
+++ app/controllers/metadata_controller.rb
@@ -0,0 +1,12 @@
+class MetadataController < ApplicationController
+                                            
+  # AJAX Controller for inserting a note
+  def create    
+    @document = eval(params[:model_name]).find params[:model_id]
+    @document.create_note_for(params[:field_name], params[:note])
+    respond_to do |format|
+      format.js { render 'notes' }
+    end
+  end
+
+end
