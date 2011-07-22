818be81b6ca95780cc41e6f8ed7aed4de713a89c
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-07-22 16:53:29 +0200

    Finalized the controller

--- app/controllers/active_metadata/metadata_controller.rb
+++ app/controllers/active_metadata/metadata_controller.rb
@@ -1,12 +1,15 @@
-class MetadataController < ApplicationController
-                                            
-  # AJAX Controller for inserting a note
-  def create    
-    @document = eval(params[:model_name]).find params[:model_id]
-    @document.create_note_for(params[:field_name], params[:note])
-    respond_to do |format|
-      format.js { render 'notes' }
+module ActiveMetadata
+  class MetadataController < ApplicationController
+                             
+    unloadable
+
+    # AJAX Controller for inserting a note
+    def create
+      @document = eval(params[:model_name]).find params[:model_id]
+      @document.create_note_for(params[:field_name], params[:note])
+      respond_to do |format|
+        format.js { render 'notes' }
+      end
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do |map|
 
   mount_at = ActiveMetadata::Engine.config.mount_at
-
-  match mount_at => 'cheese/widgets#index'
            
-  map.resource :metadata, :only => :create, :path_prefix => mount_at
+  map.resource :metadata, :only => :create,  :controller => "active_metadata/metadata", :path_prefix => mount_at
     
   # 
   # map.resources :widgets, :only => [ :index, :show ],
