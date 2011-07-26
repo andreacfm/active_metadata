99bf58a1d4d138b30d4b4cfa86fc33f808c55fe1
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-26 16:37:27 +0200

    moved methods into attachment module

--- lib/active_metadata.rb
+++ lib/active_metadata.rb
@@ -1,8 +1,7 @@
 module ActiveMetadata
-  require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3  
+  require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'active_metadata/base'
   require "active_metadata/railtie" if defined?(Rails)
   require 'application_controller'
   require "active_metadata/version"
-  require "active_metadata/attachment"
 end
