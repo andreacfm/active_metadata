503d70cc607d994232ce10394bbc3c34bff11160
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-26 11:22:18 +0200

    notes controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do |map|
 
   mount_at = ActiveMetadata::Engine.config.mount_at
            
-  map.resource :metadata, :only => :create,  :controller => "active_metadata/metadata", :path_prefix => mount_at
+  map.resource :notes, :only => :create,  :controller => "active_metadata/notes", :path_prefix => mount_at
     
   # 
   # map.resources :widgets, :only => [ :index, :show ],
