2c76fd920c391d65740f6b529be55394f4808bc9
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-26 14:26:33 +0200

    controller attachments

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do |map|
   mount_at = ActiveMetadata::Engine.config.mount_at
            
   map.resource :notes, :only => :create,  :controller => "active_metadata/notes", :path_prefix => mount_at
-    
-  # 
-  # map.resources :widgets, :only => [ :index, :show ],
-  #                         :controller => "cheese/widgets",
-  #                         
-  #                         :name_prefix => "cheese_"
+
+  map.resource :metadata_attachments, :only => :create,  :controller => "active_metadata/attachments", :path_prefix => mount_at
 
 end
