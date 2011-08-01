d65777ba38ceb97e50717ceb80ead0271676b0db
Andrea Campolonghi <acampolonghi@gmail.com>
2011-08-01 18:06:19 +0200

    controller actions

--- app/controllers/active_metadata/notes_controller.rb
+++ app/controllers/active_metadata/notes_controller.rb
@@ -1,23 +1,32 @@
 module ActiveMetadata
   class NotesController < ApplicationController
-                             
+
     unloadable
 
-    # AJAX Controller for inserting a note
+    def index
+      @document = eval(params[:model_name]).find params[:model_id] 
+      @notes = @document.notes_for params[:field_name]      
+      respond_to do |format|
+        format.html { render :layout => false}
+        format.xml  { render :xml => @notes }
+      end
+    end  
+
     def create
       @document = eval(params[:model_name]).find params[:model_id]
       @document.create_note_for(params[:field_name], params[:note])
       respond_to do |format|
-        format.js { render 'index' }
+        # TODO redirect to edit
+        format.js       
       end
     end
 
     def destroy
       @document = eval(params[:model_name]).find params[:model_id]
-      @document.delete_note(params[:id])
-      @document.reload
+      @document.delete_note_for(params[:field_name],params[:id])
       respond_to do |format|
-        format.js { render 'index' }
+        # TODO redirect to index
+        format.js
       end
     end
 
