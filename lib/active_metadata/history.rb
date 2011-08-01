f2673f63583ccfbbf92a32a8ff078aa8ced14890
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-08-01 14:37:00 +0200

    Minor Changes

--- lib/active_metadata/history.rb
+++ lib/active_metadata/history.rb
@@ -5,6 +5,7 @@ module ActiveMetadata::History
     def save_history
       self.changes.each do |key, value|
         next if ActiveMetadata::CONFIG['history_skip_fields'].include?(key)
+        
         label = ActiveMeta.find_or_create_by(:document_id => metadata_id).labels.find_or_create_by(:name => key.to_s)
         label.histories.create!(:value => value[1], :created_at => Time.now.utc)
       end
@@ -12,6 +13,7 @@ module ActiveMetadata::History
 
     def history_for field
       meta = ActiveMeta.find_or_create_by(:document_id => metadata_id)
+      
       label = meta.labels.find_or_create_by(:name => field.to_s)
       label.histories.desc(:created_at).to_a
     end
