4ba88f4b4958b7b21a1d33158a8c96fc8dc78469
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-09-06 19:01:44 +0200

    Deleted moved models

--- app/models/inbox.rb
+++
@@ -1,7 +0,0 @@
-class Inbox < ActiveRecord::Base
-  set_table_name :inboxes
-  
-  belongs_to :user 
-  has_many :messages
-end
-  
--- app/models/message.rb
+++
@@ -1,9 +0,0 @@
-class Message < ActiveRecord::Base
-  set_table_name :alert_messages
-  
-  belongs_to :inbox     
-  
-  def mark_as_read
-    update_attribute(:read, true)
-  end
-end
