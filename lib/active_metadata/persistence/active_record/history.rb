c029179af034fee8e0a57e836fd2d509c37fb8e5
Andrea Campolonghi <acampolonghi@gmail.com>
2011-09-19 16:19:56 +0200

    removed callback for on save for history history has now a send_notification

--- lib/active_metadata/persistence/active_record/history.rb
+++ lib/active_metadata/persistence/active_record/history.rb
@@ -10,6 +10,7 @@ module ActiveMetadata::Persistence::ActiveRecord::History
       self.changes.each do |key, value|
         next if ActiveMetadata::CONFIG['history_skip_fields'].include?(key)        
         History.create! :value => value[1],:document_id => metadata_id,:label => key.to_s 
+        self.send(:send_notification, key, value[0], value[1], :history_message) 
       end
     end
 
