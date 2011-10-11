cac75f4f93cf81afd44e4f394adb8b32d4fefbbc
Andrea Campolonghi <acampolonghi@gmail.com>
2011-10-11 13:48:34 +0200

    implements caching in notes

--- lib/active_metadata/persistence/active_record/note.rb
+++ lib/active_metadata/persistence/active_record/note.rb
@@ -7,20 +7,23 @@ module ActiveMetadata::Persistence::ActiveRecord::Note
   module InstanceMethods
     
     def create_note_for(field, note)      
-      Note.create! :document_id => metadata_id,:label => field.to_s,:note => note, :created_by => current_user_id    
+      Note.create! :document_id => metadata_id,:label => field.to_s,:note => note, :created_by => current_user_id  
+      invalidate_cache_for field 
       self.send(:send_notification, field, "", note, :note_message, current_user_id) 
     end
 
     def update_note(id, note)
       n = Note.find(id)
       old_value = n.note
-      n.update_attributes! :note => note, :updated_by => current_user_id, :updated_at => Time.now.utc
-      
+      n.update_attributes! :note => note, :updated_by => current_user_id, :updated_at => Time.now.utc     
+      invalidate_cache_for field 
       self.send(:send_notification, n.label, old_value, note, :note_message, current_user_id)
     end
 
     def notes_for(field)
-      Note.all(:conditions => {:label => field, :document_id => metadata_id}, :order => "updated_at DESC" )
+      Rails.cache.fetch("active_metadata/notes/#{field}", :expires_in => 5.minutes) do
+        Note.all(:conditions => {:label => field, :document_id => metadata_id}, :order => "updated_at DESC" )        
+      end    
     end
 
     def note_for(field,id)
@@ -35,11 +38,19 @@ module ActiveMetadata::Persistence::ActiveRecord::Note
       n = Note.find(id)
       old_value = n.note
       n.destroy
+      invalidate_cache_for field 
       self.send(:send_notification, field, old_value, "", :note_message)
     end
     
-    def has_notes_for field
-      Note.count(:conditions => {:label => field, :document_id => metadata_id}) == 0 ? false : true
+    def has_notes_for field      
+      notes_for(field).size == 0 ? false : true
     end
+    
+    private
+    
+    def invalidate_cache_for field
+      Rails.cache.delete("active_metadata/notes/#{field}")     
+    end  
+    
   end
 end
