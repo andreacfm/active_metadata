8b603cef8e29fa9b1cb67299cf5f10767383787a
Andrea Campolonghi <acampolonghi@gmail.com>
2011-10-13 11:28:25 +0200

    write through caching for attachments

--- lib/active_metadata/persistence/active_record/attachment.rb
+++ lib/active_metadata/persistence/active_record/attachment.rb
@@ -8,17 +8,21 @@ module ActiveMetadata::Persistence::ActiveRecord::Attachment
 
     def save_attachment_for(field, file)      
       attachment = Attachment.create! :document_id => metadata_id, :label => field, :attach => file, :created_by => current_user_id       
+      reload_attachments_cache_for field 
       self.send(:send_notification, field, "", attachment.attach.original_filename, :attachment_message, current_user_id)
     end
 
     def attachments_for(field)
-      Attachment.all(:conditions => {:document_id => metadata_id,:label => field}, :order => "attach_updated_at DESC")
+      Rails.cache.fetch(attachments_cache_key(field), :expires_in => ActiveMetadata::CONFIG['cache_expires_in'].minutes) do
+        fetch_attachments_for field   
+      end  
     end
 
     def delete_attachment_for(field,id)
       a = Attachment.find(id)
       filename = a.attach.original_filename
       a.destroy      
+      reload_attachments_cache_for field 
       self.send(:send_notification, field, filename, "", :attachment_message)
     end
 
@@ -29,13 +33,25 @@ module ActiveMetadata::Persistence::ActiveRecord::Attachment
       a.updated_by = current_user_id
       a.save                                            
       new_filename = a.attach.original_filename
-      
+
+      reload_attachments_cache_for field       
       self.send(:send_notification, field, old_filename, new_filename, :attachment_message, current_user_id)
     end
 
     def has_attachments_for field
-      Attachment.count(:conditions => {:label => field, :document_id => metadata_id}) == 0 ? false : true
+      attachments_for(field).size == 0 ? false : true
     end
+    
+    private
+    
+    def reload_attachments_cache_for field
+      Rails.cache.write(attachments_cache_key(field),fetch_attachments_for(field), :expires_in => ActiveMetadata::CONFIG['cache_expires_in'].minutes )     
+    end  
+    
+    def fetch_attachments_for field
+      Attachment.all(:conditions => {:document_id => metadata_id,:label => field}, :order => "attach_updated_at DESC")
+    end  
+    
 
   end
 
