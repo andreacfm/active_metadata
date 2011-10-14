b59106a447a3f358b2bf28be412acb59e00e2811
Andrea Campolonghi <acampolonghi@gmail.com>
2011-10-14 17:49:55 +0200

    removed mongoid models

--- lib/model/mongoid/active_meta.rb
+++
@@ -1,6 +0,0 @@
-class ActiveMeta
-  include Mongoid::Document
-  field :document_id, type: Integer
-  has_many :labels, :class_name => "Label"
-  index :document_id
-end
--- lib/model/mongoid/history.rb
+++
@@ -1,9 +0,0 @@
-class History
-  include Mongoid::Document
-  store_in :history
-  
-  belongs_to :label
-
-  field :value, :type => String
-
-end
