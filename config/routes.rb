516086ed8b7fb33a24dd8c1d6bdc72e5011bf19a
Gian Carlo Pace <giancarlo.pace@gmail.com>
2012-04-27 16:19:20 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,31 @@ Rails.application.routes.draw do
   namespace :active_metadata do
     #stream
     get ":model_name/:model_id/:field_name/stream" => 'stream#index', :as => 'stream'
+    #get "group/:group_name/stream" => 'stream#index', :as => 'group_stream'
+    #get "group/:group_name/stream/starred" => 'stream#index', :as => 'group_stream_starred'
 
     #notes
-    match ':model_name/:model_id/:field_name/notes' => 'notes#index', :via => :get, :as => "notes"
-    match ':model_name/:model_id/:field_name/notes' => 'notes#create', :via => :post, :as => "create_note"
-    match ':model_name/:model_id/:field_name/notes/:id' => 'notes#destroy', :via => :delete, :as => "destroy_note"
-    match ':model_name/:model_id/:field_name/notes/:id/edit' => 'notes#edit', :via => :get, :as => "edit_note"
-    match ':model_name/:model_id/:field_name/notes/:id' => 'notes#update', :via => :put, :as => "update_note"
-    match ':model_name/:model_id/:field_name/notes/:id' => 'notes#show', :via => :get, :as => "show_note"
-    match ':model_name/:model_id/:field_name/notes/starred' => 'notes#starred', :via => :get, :as => "starred_notes"
-    match ':model_name/:model_id/:field_name/notes/:id/star' => 'notes#star', :via => :put, :as => "star_note"
-    match ':model_name/:model_id/:field_name/notes/:id/unstar' => 'notes#unstar', :via => :put, :as => "unstar_note"
+    get ':model_name/:model_id/:field_name/notes' => 'notes#index', :as => "notes"
+    post ':model_name/:model_id/:field_name/notes' => 'notes#create', :as => "create_note"
+    delete ':model_name/:model_id/:field_name/notes/:id' => 'notes#destroy', :as => "destroy_note"
+    get ':model_name/:model_id/:field_name/notes/:id/edit' => 'notes#edit', :as => "edit_note"
+    put ':model_name/:model_id/:field_name/notes/:id' => 'notes#update', :as => "update_note"
+    get ':model_name/:model_id/:field_name/notes/:id' => 'notes#show', :as => "show_note"
+    get ':model_name/:model_id/:field_name/notes/starred' => 'notes#starred', :as => "starred_notes"
+    put ':model_name/:model_id/:field_name/notes/:id/star' => 'notes#star', :as => "star_note"
+    put ':model_name/:model_id/:field_name/notes/:id/unstar' => 'notes#unstar', :as => "unstar_note"
 
     #history
     match ':model_name/:model_id/:field_name/histories' => 'histories#index', :via => :get, :as => "histories"
 
     #attachments
-    match ':model_name/:model_id/:field_name/attachments' => 'attachments#index', :via => :get, :as => "attachments"
-    match ':model_name/:model_id/:field_name/attachments' => 'attachments#create', :via => :post, :as => "create_attachment"
-    match ':model_name/:model_id/:field_name/attachments/:id' => 'attachments#destroy', :via => :delete, :as => "destroy_attachment"
-    match ':model_name/:model_id/:field_name/attachments/:id' => 'attachments#update', :via => :put, :as => "update_attachment"
-    match ':model_name/:model_id/:field_name/attachments/starred' => 'attachments#starred', :via => :get, :as => "starred_attachments"
-    match ':model_name/:model_id/:field_name/attachments/:id/star' => 'attachments#star', :via => :put, :as => "star_attachment"
-    match ':model_name/:model_id/:field_name/attachments/:id/unstar' => 'attachments#unstar', :via => :put, :as => "unstar_attachment"
+    get ':model_name/:model_id/:field_name/attachments' => 'attachments#index', :as => "attachments"
+    post ':model_name/:model_id/:field_name/attachments' => 'attachments#create', :as => "create_attachment"
+    delete ':model_name/:model_id/:field_name/attachments/:id' => 'attachments#destroy', :as => "destroy_attachment"
+    put ':model_name/:model_id/:field_name/attachments/:id' => 'attachments#update', :as => "update_attachment"
+    get ':model_name/:model_id/:field_name/attachments/starred' => 'attachments#starred', :as => "starred_attachments"
+    put ':model_name/:model_id/:field_name/attachments/:id/star' => 'attachments#star', :as => "star_attachments"
+    put ':model_name/:model_id/:field_name/attachments/:id/unstar' => 'attachments#unstar', :as => "unstar_attachments"
 
     #alerts
     get ':model_name/:model_id/:field_name/watchers' => 'watchers#index', :as => "watchers"
